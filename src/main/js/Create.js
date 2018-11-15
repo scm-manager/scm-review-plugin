3d065b80f5a7cdd87e5b1e0307bd30d65305de6d
Maren Süwer <maren.suewer@cloudogu.com>
2018-11-15 13:57:46 +0100

    add error handling for creating a pull request, add loading state for Select components, set source and target at beginning and verify if everything is set before submit button is enabled

--- src/main/js/Create.js
+++ src/main/js/Create.js
@@ -1,9 +1,16 @@
 //@flow
 import React from "react";
-import {apiClient, Title, Subtitle, SubmitButton} from "@scm-manager/ui-components";
-import type {Repository} from "@scm-manager/ui-types";
-import CreateForm from './CreateForm';
+import {
+  apiClient,
+  Title,
+  Subtitle,
+  SubmitButton,
+  ErrorNotification
+} from "@scm-manager/ui-components";
+import type { Repository } from "@scm-manager/ui-types";
+import CreateForm from "./CreateForm";
 import injectSheet from "react-jss";
+import type {PullRequest} from "./PullRequest";
 
 const styles = {
   controlButtons: {
@@ -17,17 +24,18 @@ type Props = {
 };
 
 type State = {
-  pullRequest?: Object, // TODO add type
+  pullRequest?: PullRequest,
   loading: boolean,
-  error?: Error
+  error?: Error,
+  disabled: boolean
 };
 
 class Create extends React.Component<Props, State> {
-
   constructor(props: Props) {
     super(props);
     this.state = {
-      loading: false
+      loading: false,
+      disabled: true
     };
   }
 
@@ -35,37 +43,53 @@ class Create extends React.Component<Props, State> {
     const { pullRequest } = this.state;
     const { repository } = this.props;
 
-    //this.setState({loading: true});
-    // TODO handle loading, success and error
-    apiClient.post(repository._links.newPullRequest.href, pullRequest)
+    this.setState({ loading: true });
+    // TODO handle loading, success
+    apiClient
+      .post(repository._links.newPullRequest.href, pullRequest)
+      .then(this.setState({ loading: false }))
       .catch(cause => {
-        const error = new Error(`could not fetch users: ${cause.message}`);
-        this.setState({error: error});
-      });;
+        const error = new Error(
+          `could not create pull request: ${cause.message}`
+        );
+        this.setState({ error: error });
+      });
   };
 
-  handleFormChange = (pullRequest) => {
+  verify = (pullRequest: PullRequest) => {
+    if(pullRequest.target && pullRequest.source && pullRequest.title){
+      return true;
+    }
+    return false;
+  };
+
+  handleFormChange = (pullRequest: PullRequest) => {
     this.setState({
-      pullRequest
+      pullRequest,
+      disabled: !this.verify(pullRequest)
     });
   };
 
   render() {
-    const {repository, classes} = this.props;
-    const {loading, error} = this.state;
-  console.log(this.state.pullRequest);
+    const { repository, classes } = this.props;
+    const { loading, error, disabled } = this.state;
 
-  if(error)
-    return <ErrorNotification error={error}/>;
+    const errorNotification = error ? (
+      <ErrorNotification error={error} />
+    ) : null;
 
     return (
       <div className="columns">
         <div className="column">
-
-          <Title title="Pull Request"/>
-          <Subtitle subtitle={"Creates a new Pull Request for " + repository.name}/>
-
-          <CreateForm repository={repository} onChange={this.handleFormChange}/>
+          <Title title="Pull Request" />
+          <Subtitle
+            subtitle={"Creates a new Pull Request for " + repository.name}
+          />
+          {errorNotification}
+          <CreateForm
+            repository={repository}
+            onChange={this.handleFormChange}
+          />
 
           <div className="tabs">
             <ul>
@@ -78,18 +102,20 @@ class Create extends React.Component<Props, State> {
             </ul>
           </div>
 
-          <p>
-            The Changelog ...
-          </p>
+          <p>The Changelog ...</p>
 
           <div className={classes.controlButtons}>
-            <SubmitButton label="Submit" action={this.submit} loading={loading}/>
+            <SubmitButton
+              label="Submit"
+              action={this.submit}
+              loading={loading}
+              disabled={disabled}
+            />
           </div>
         </div>
       </div>
     );
   }
-
 }
 
 export default injectSheet(styles)(Create);
--- src/main/js/CreateForm.js
+++ src/main/js/CreateForm.js
@@ -2,18 +2,17 @@
 import React from "react";
 import type { Repository } from "@scm-manager/ui-types";
 import {apiClient, InputField, Textarea, Select} from "@scm-manager/ui-components";
+import type {PullRequest} from "./PullRequest";
 
 type Props = {
   repository: Repository,
-  onChange: (pr: Object) => void // todo add type
+  onChange: (pr: PullRequest) => void
 };
 
 type State = {
+  pullRequest?: PullRequest,
   branches: string[],
-  source?: string,
-  target?: string,
-  title?: string,
-  description?: string
+  loading: boolean
 };
 
 class CreateForm extends React.Component<Props, State> {
@@ -21,48 +20,60 @@ class CreateForm extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      branches: []
+      branches: [],
+      loading: true
     }
   }
 
   componentDidMount() {
     const { repository } = this.props;
-    // TODO set loading state for the selects
     // TODO error handling
     apiClient.get(repository._links.branches.href)
       .then(response => response.json())
       .then(collection => collection._embedded.branches)
       .then(branches => branches.map(b => b.name))
-      .then(names => this.setState({branches: names}));
+      .then(names => this.setState(
+        {
+          branches: names,
+          loading: false,
+          pullRequest: {
+            source: names[0], //set first entry, otherwise nothing is select in state even if one branch is shown in Select component at beginning
+            target: names[0]
+          }
+        }
+        ));
   }
 
   handleFormChange = (value: string, name: string) => {
     this.setState({
-      [name]: value
+      pullRequest: {
+        ...this.state.pullRequest,
+        [name]: value
+      }
     }, this.notifyAboutChangedForm);
   };
 
   notifyAboutChangedForm = () => {
-    const pullRequest = { ...this.state, branches: undefined }; // we don't need the branches
+    const pullRequest = { ...this.state.pullRequest};
     this.props.onChange(pullRequest);
   };
 
   render() {
+    const {loading} = this.state;
     const options = this.state.branches.map((branch) => ({
       label: branch,
       value: branch
     }));
 
-    // TODO fix form submit
     return (
       <form>
 
         <div className="columns">
           <div className="column">
-            <Select name="source" label="Source Branch" options={options} onChange={this.handleFormChange}/>
+            <Select name="source" label="Source Branch" options={options} onChange={this.handleFormChange} loading={loading}/>
           </div>
           <div className="column">
-            <Select name="target" label="Target Branch" options={options} onChange={this.handleFormChange}/>
+            <Select name="target" label="Target Branch" options={options} onChange={this.handleFormChange} loading={loading}/>
           </div>
         </div>
 
