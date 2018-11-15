f732c8c8c7883c7573f4cd428a7b332d61400c6a
Maren Süwer <maren.suewer@cloudogu.com>
2018-11-15 15:42:07 +0100

    outsourcing api calls

---
+++ src/main/js/pullRequest.js
@@ -0,0 +1,27 @@
+import type {PullRequest} from "./PullRequest";
+import {apiClient} from "@scm-manager/ui-components";
+
+export function createPullRequest(url: string, pullRequest: PullRequest) {
+  return apiClient
+    .post(url, pullRequest)
+    .then(response => { return response; })
+    .catch(cause => {
+      const error = new Error(
+        `could not create pull request: ${cause.message}`
+      );
+      return { error: error };
+    });
+}
+
+export function getBranches(url: string) {
+  return apiClient.get(url)
+    .then(response => response.json())
+    .then(collection => collection._embedded.branches)
+    .then(branches => { return branches.map(b => b.name); })
+    .catch(cause => {
+      const error = new Error(
+        `could not fetch branches: ${cause.message}`
+      );
+      return { error: error };
+    });
+}
---
+++ src/main/js/pullRequest.test.js
@@ -0,0 +1,80 @@
+// @flow
+import fetchMock from "fetch-mock";
+import type { PullRequest } from "./PullRequest";
+import { createPullRequest, getBranches } from "./pullRequest";
+
+describe("API create pull request", () => {
+  const PULLREQUEST_URL = "/repositories/scmadmin/TestRepo/newPullRequest";
+
+  const BRANCH_URL = "/repositories/scmadmin/TestRepo/branches";
+
+  const pullRequest: PullRequest = {
+    source: "sourceBranch",
+    target: "targetBranch",
+    title: "This is a title"
+  };
+
+  const branchRequest = {
+    _embedded: {
+      branches: [
+        {
+          name: "branchA"
+        },
+        {
+          name: "branchB"
+        }
+      ]
+    }
+  };
+
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+
+  it("should create pull request successfully", done => {
+    fetchMock.postOnce("/api/v2" + PULLREQUEST_URL, {
+      status: 204
+    });
+
+    createPullRequest(PULLREQUEST_URL, pullRequest).then(response => {
+      expect(response.status).toBe(204);
+      expect(response.error).toBeUndefined();
+      done();
+    });
+  });
+
+  it("should fail on creating pull request", () => {
+    fetchMock.postOnce("/api/v2" + PULLREQUEST_URL, {
+      status: 500
+    });
+
+    createPullRequest(PULLREQUEST_URL, pullRequest).then(response => {
+      expect(response.error).toBeDefined();
+      done();
+    });
+  });
+
+  it("should get branches successfully", done => {
+    fetchMock.getOnce("/api/v2" + BRANCH_URL,
+      branchRequest
+    );
+
+    getBranches(BRANCH_URL).then(response => {
+      expect(response).toEqual(["branchA", "branchB"]);
+      expect(response.error).toBeUndefined();
+      done();
+    });
+  });
+
+  it("should fail on getting branches", () => {
+    fetchMock.getOnce("/api/v2" + BRANCH_URL, {
+      status: 500
+    });
+
+    getBranches(BRANCH_URL).then(response => {
+      expect(response.error).toBeDefined();
+      done();
+    });
+  });
+});
