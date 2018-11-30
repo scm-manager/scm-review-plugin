b274aca76657d3336896d5e2ab720580c5e0bae2
Mohamed Karray <mohamed.karray.sr@gmail.com>
2018-11-30 15:03:04 +0100

    Endpoint to get all pullRequests

--- src/test/java/com/cloudogu/scm/review/TestData.java
+++ src/test/java/com/cloudogu/scm/review/TestData.java
@@ -5,11 +5,7 @@ import java.time.Instant;
 public class TestData {
 
   public static PullRequest createPullRequest() {
-    PullRequest pullRequest = new PullRequest("develop", "master", "Awesome PR");
-    pullRequest.setDescription("Hitchhiker's guide to the galaxy");
-    pullRequest.setAuthor("dent");
-    pullRequest.setCreationDate(Instant.MIN);
-    return pullRequest;
+    return createPullRequest("id");
   }
 
   public static PullRequest createPullRequest(String id) {
