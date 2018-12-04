13eb9ca2a0e21b547b80379b7a469d053be96b3e
Mohamed Karray <mohamed.karray.sr@gmail.com>
2018-12-04 17:01:01 +0100

    remove @Size from the PullRequest Class

--- src/main/java/com/cloudogu/scm/review/PullRequest.java
+++ src/main/java/com/cloudogu/scm/review/PullRequest.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -19,11 +18,8 @@ import java.time.Instant;
 public class PullRequest {
 
   private String id;
-  @Size(min = 1)
   private String source;
-  @Size(min = 1)
   private String target;
-  @Size(min = 1)
   private String title;
   private String description;
   private String author;
