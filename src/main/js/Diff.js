efdac0eee3d12b26b234a6a9bfc20d8d22881b63
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2018-12-11 14:14:03 +0100

    implemented diff

--- src/main/js/Diff.js
+++ src/main/js/Diff.js
@@ -1,23 +1,31 @@
 //@flow
 import React from 'react';
+import { LoadingDiff, Notification } from "@scm-manager/ui-components";
 import type {Repository} from "@scm-manager/ui-types";
+import {createDiffUrl} from "./pullRequest";
+import { translate } from "react-i18next";
 
 type Props = {
   repository: Repository,
   source: string,
-  target: string
+  target: string,
+
+  //context props
+  t: string => string
 }
 
 class Diff extends React.Component<Props> {
 
   render() {
-    return (
-      <div>
-        <h1>Diff</h1>
-      </div>
-    );
+    const { repository, source, target, t } = this.props;
+    const url = createDiffUrl(repository, source, target);
+    if (!url) {
+      return <Notification type="danger">{t("scm-review-plugin.diff.not-supported")}</Notification>;
+    } else {
+      return <LoadingDiff url={url} />;
+    }
   }
 
 }
 
-export default Diff;
+export default translate("plugins")(Diff);
