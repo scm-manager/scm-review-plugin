099dbe50a59d5512f501568856257a6ccf6162df
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2018-12-11 20:21:44 +0100

    Suppress sonar issue

--- src/main/java/com/cloudogu/scm/review/StatusChangeNotAllowedException.java
+++ src/main/java/com/cloudogu/scm/review/StatusChangeNotAllowedException.java
@@ -6,6 +6,7 @@ import sonia.scm.repository.Repository;
 
 import static sonia.scm.ContextEntry.ContextBuilder.entity;
 
+@SuppressWarnings("squid:MaximumInheritanceDepth") // exceptions have a deep inheritance depth themselves; therefore we accept this here
 public class StatusChangeNotAllowedException extends BadRequestException {
   public StatusChangeNotAllowedException(Repository repository, PullRequest pullRequest) {
     super(entity(PullRequest.class, pullRequest.getId()).in(repository).build(), "illegal status change");
