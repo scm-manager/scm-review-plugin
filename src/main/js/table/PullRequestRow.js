e6010d2ce6aafb62bc57dedbabd6f9a9943c6fc1
Florian Scholdei <florian.scholdei@cloudogu.com>
2018-12-14 12:07:53 +0100

    implemented word-break for column

--- src/main/js/table/PullRequestRow.js
+++ src/main/js/table/PullRequestRow.js
@@ -1,30 +1,51 @@
 // @flow
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import type { PullRequest } from "./../types/PullRequest";
-import {DateFromNow} from "@scm-manager/ui-components";
+import { DateFromNow } from "@scm-manager/ui-components";
+import injectSheet from "react-jss";
+
+const styles = {
+  wordBreakColumn: {
+    WebkitHyphens: "auto",
+    MozHyphens: "auto",
+    MsHyphens: "auto",
+    hypens: "auto",
+    wordBreak: "break-all",
+    minWidth: "10em"
+  }
+};
 
 type Props = {
+  classes: any,
   pullRequest: PullRequest
 };
 
-export default class PullRequestRow extends React.Component<Props> {
+class PullRequestRow extends React.Component<Props> {
   renderLink(to: string, label: string) {
     return <Link to={to}>{label}</Link>;
   }
 
   render() {
-    const { pullRequest } = this.props;
+    const { classes, pullRequest } = this.props;
     const to = `pull-request/${pullRequest.id}/changesets/`;
     return (
       <tr>
-        <td>{this.renderLink(to, pullRequest.title)}</td>
-        <td>{pullRequest.source}</td>
-        <td>
-          {pullRequest.target}
+        <td className={classes.wordBreakColumn}>
+          {this.renderLink(to, pullRequest.title)}
         </td>
-        <td className="is-hidden-mobile">{pullRequest.author ? pullRequest.author : ""}</td>
-        <td className="is-hidden-mobile">{pullRequest.creationDate ? <DateFromNow date={pullRequest.creationDate} />: ""}</td>
+        <td className={classes.wordBreakColumn}>{pullRequest.source}</td>
+        <td className={classes.wordBreakColumn}>{pullRequest.target}</td>
+        <td className="is-hidden-mobile">
+          {pullRequest.author ? pullRequest.author : ""}
+        </td>
+        <td className="is-hidden-mobile">
+          {pullRequest.creationDate ? (
+            <DateFromNow date={pullRequest.creationDate} />
+          ) : (
+            ""
+          )}
+        </td>
         <td className="is-hidden-mobile">
           {pullRequest.status ? pullRequest.status : ""}
         </td>
@@ -32,3 +53,5 @@ export default class PullRequestRow extends React.Component<Props> {
     );
   }
 }
+
+export default withRouter(injectSheet(styles)(PullRequestRow));
