ceca31b608296c78dc006bc4f41685c7a2dd5265
Florian Scholdei <florian.scholdei@cloudogu.com>
2019-01-16 11:16:16 +0100

    added NavLink component

---
+++ src/main/js/PullRequestsNavLink.js
@@ -0,0 +1,27 @@
+// @flow
+import React from "react";
+import { NavLink } from "@scm-manager/ui-components";
+import { translate } from "react-i18next";
+
+type Props = {
+  url: string,
+  activeWhenMatch: (route: any) => boolean,
+  t: string => string
+};
+
+class PullRequestsNavLink extends React.Component<Props> {
+  render() {
+    const { url, activeWhenMatch, t } = this.props;
+
+    return (
+      <NavLink
+        to={`${url}/pull-requests`}
+        icon="fas fa-chart-pie"
+        label={t("scm-review-plugin.navLink")}
+        activeWhenMatch={activeWhenMatch}
+      />
+    );
+  }
+}
+
+export default translate("plugins")(PullRequestsNavLink);
