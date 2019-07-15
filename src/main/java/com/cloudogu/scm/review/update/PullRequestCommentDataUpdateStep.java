9e15ebfd37af74d779923ee09ccdccbc6987758e
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-07-15 13:20:17 +0200

    add UpdateStepTest

--- src/main/java/com/cloudogu/scm/review/update/PullRequestCommentDataUpdateStep.java
+++ src/main/java/com/cloudogu/scm/review/update/PullRequestCommentDataUpdateStep.java
@@ -1,6 +1,7 @@
 package com.cloudogu.scm.review.update;
 
 import com.cloudogu.scm.review.comment.service.CommentType;
+import com.cloudogu.scm.review.comment.service.PullRequestComments;
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,27 +9,37 @@ import sonia.scm.migration.UpdateStep;
 import sonia.scm.plugin.Extension;
 import sonia.scm.repository.Repository;
 import sonia.scm.repository.xml.XmlRepositoryDAO;
+import sonia.scm.store.DataStore;
+import sonia.scm.store.DataStoreFactory;
 import sonia.scm.version.Version;
 
 @Extension
 public class PullRequestCommentDataUpdateStep implements UpdateStep {
 
   private static final Logger LOG = LoggerFactory.getLogger(PullRequestCommentDataUpdateStep.class);
+  private static final String STORE_NAME = "pullRequestComment";
   private final XmlRepositoryDAO repositoryDAO;
+  private final DataStoreFactory dataStoreFactory;
 
   @Inject
-  public PullRequestCommentDataUpdateStep(XmlRepositoryDAO repositoryDAO) {
+  public PullRequestCommentDataUpdateStep(XmlRepositoryDAO repositoryDAO, DataStoreFactory dataStoreFactory) {
     this.repositoryDAO = repositoryDAO;
+    this.dataStoreFactory = dataStoreFactory;
   }
 
   @Override
   public void doUpdate() {
-      for(Repository repository : repositoryDAO.getAll()) {
-        if (repository.getType() == null) {
-          LOG.debug("update pullrequest comments to new data structure for repository id {}", repository.getId());
-          repository.setType(String.valueOf(CommentType.COMMENT));
+    for(Repository repository : repositoryDAO.getAll()) {
+      DataStore<PullRequestComments> dataStore = dataStoreFactory.withType(PullRequestComments.class).withName(STORE_NAME).forRepository(repository).build();
+      LOG.debug("update pullrequest comments for repository with id {}", repository.getId());
+      dataStore.getAll().forEach((key, properties) -> properties.getComments().forEach(comment -> {
+        if (comment.getType() == null) {
+          comment.setType(CommentType.COMMENT);
         }
-      }
+        dataStore.put(key, properties);
+      })
+      );
+    }
   }
 
   @Override
@@ -38,6 +49,6 @@ public class PullRequestCommentDataUpdateStep implements UpdateStep {
 
   @Override
   public String getAffectedDataType() {
-    return "sonia.scm.review.pullrequest.comment.data.xml";
+    return "sonia.scm.pullrequest.comment.data.xml";
   }
 }
---
+++ src/test/java/com/cloudogu/scm/review/update/PullRequestCommentDataUpdateStepTest.java
@@ -0,0 +1,77 @@
+package com.cloudogu.scm.review.update;
+
+import com.cloudogu.scm.review.comment.service.Comment;
+import com.cloudogu.scm.review.comment.service.CommentType;
+import com.cloudogu.scm.review.comment.service.PullRequestComments;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sonia.scm.repository.Repository;
+import sonia.scm.repository.xml.XmlRepositoryDAO;
+import sonia.scm.store.DataStore;
+import sonia.scm.store.DataStoreFactory;
+import sonia.scm.store.InMemoryDataStore;
+import sonia.scm.store.InMemoryDataStoreFactory;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static sonia.scm.repository.RepositoryTestData.createHeartOfGold;
+
+@ExtendWith(MockitoExtension.class)
+class PullRequestCommentDataUpdateStepTest {
+
+  @Mock
+  XmlRepositoryDAO repositoryDAO;
+
+  private PullRequestCommentDataUpdateStep updateStep;
+  private DataStoreFactory dataStoreFactory = new InMemoryDataStoreFactory(new InMemoryDataStore());
+  private DataStore<PullRequestComments> dataStore;
+  private Repository repository = createHeartOfGold();
+
+  private final static String STORE_NAME = "pullRequestComment";
+
+  @BeforeEach
+  void init() {
+    dataStore = dataStoreFactory.withType(PullRequestComments.class).withName(STORE_NAME).forRepository(repository).build();
+    updateStep = new PullRequestCommentDataUpdateStep(repositoryDAO, dataStoreFactory);
+  }
+
+  @BeforeEach
+  void mockRepositories() {
+    when(repositoryDAO.getAll()).thenReturn(Collections.singletonList(repository));
+  }
+
+  @Test
+  void shouldUpdatePullRequestCommentData() {
+    Comment comment1 = new Comment();
+    comment1.setComment("comment1");
+    comment1.setAuthor("trillian");
+    comment1.setType(CommentType.COMMENT);
+    Comment comment2 = new Comment();
+    comment2.setAuthor("edi");
+    comment2.setComment("comment2");
+    comment2.setType(null);
+    PullRequestComments comments = new PullRequestComments();
+    comments.setComments(Arrays.asList(comment1, comment2));
+    dataStore.put(comments);
+
+    updateStep.doUpdate();
+
+    assertThat(dataStore.getAll().values().iterator().next().getComments().size()).isEqualTo(2);
+    assertThat(dataStore.getAll().values().iterator().next().getComments().get(0).getType()).isEqualTo(CommentType.COMMENT);
+    assertThat(dataStore.getAll().values().iterator().next().getComments().get(1).getType()).isEqualTo(CommentType.COMMENT);
+  }
+
+  @Test
+  void shouldSkipIfNoDataAvailable() {
+    updateStep.doUpdate();
+
+    assertThat(dataStore.getAll().size()).isEqualTo(0);
+  }
+}
+
