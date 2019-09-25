176c8109005fa56e59f47ab1554f0551d9d23dcc
Florian Scholdei <florian.scholdei@cloudogu.com>
2019-09-25 14:03:41 +0200

    use tagGroup instead

--- src/main/js/types/PullRequest.js
+++ src/main/js/types/PullRequest.js
@@ -1,16 +1,10 @@
 // @flow
-import type { Collection, Links } from "@scm-manager/ui-types";
+import type { DisplayedUser, Links, Collection } from "@scm-manager/ui-types";
 
 export type BasicPullRequest = {
   source: string,
   target: string,
   title: string
-};
-
-export type DisplayedUser = {
-  id: string,
-  displayName: string,
-  mail: string
 };
 
 export type PullRequest = BasicPullRequest & {
@@ -51,13 +45,13 @@ export type Comment = BasicComment & {
 
 export type Context = {
   lines: ContextLine[]
-}
+};
 
 export type ContextLine = {
   content: string,
   newLineNumber?: number,
   oldLineNumber?: number
-}
+};
 
 export type Reply = BasicComment & {
   author: DisplayedUser,
@@ -80,4 +74,4 @@ export type Comments = Collection & {
 export type PossibleTransition = {
   name: string,
   _links: Links
-}
+};
