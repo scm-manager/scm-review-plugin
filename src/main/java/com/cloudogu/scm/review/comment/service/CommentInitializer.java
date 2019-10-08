85a7347243d9865d8bb62cd5f15040c332bee9d2
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-10-08 13:28:07 +0200

    Remove irrelevant checks

--- src/main/java/com/cloudogu/scm/review/comment/service/CommentInitializer.java
+++ src/main/java/com/cloudogu/scm/review/comment/service/CommentInitializer.java
@@ -85,12 +85,8 @@ public class CommentInitializer {
 
   private DiffFile findMatchingDiffFile(Comment comment, DiffResult diffResult) {
     for (DiffFile diffFile : diffResult) {
-      if (diffFile.getNewPath() != null) {
-        if (diffFile.getOldPath().equals(comment.getLocation().getFile())) {
-          return diffFile;
-        }
-      }
-      if (diffFile.getNewPath().equals(comment.getLocation().getFile())) {
+      if (diffFile.getNewPath().equals(comment.getLocation().getFile())
+        || diffFile.getOldPath().equals(comment.getLocation().getFile())) {
         return diffFile;
       }
     }
