3c3d15e35664924d25f7d6458f5a2f76919c196d
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-11-28 10:24:56 +0100

    Do not remove line in update step
    Without this, the changeId field for inline comments in legacy comment
    data will be removed, because the new store type does not have this
    field. Therefore the following update step migrating these changeIds to
    line numbers has nothing to migrate, which will lead to errors in
    displaying these comments.

--- src/main/java/com/cloudogu/scm/review/update/AddTypeToCommentUpdateStep.java
+++ src/main/java/com/cloudogu/scm/review/update/AddTypeToCommentUpdateStep.java
@@ -1,46 +1,126 @@
 package com.cloudogu.scm.review.update;
 
-import com.cloudogu.scm.review.comment.service.CommentType;
-import com.cloudogu.scm.review.comment.service.PullRequestComments;
 import com.google.inject.Inject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import sonia.scm.migration.UpdateException;
 import sonia.scm.migration.UpdateStep;
 import sonia.scm.plugin.Extension;
-import sonia.scm.repository.Repository;
-import sonia.scm.repository.RepositoryDAO;
-import sonia.scm.repository.xml.XmlRepositoryDAO;
-import sonia.scm.store.DataStore;
-import sonia.scm.store.DataStoreFactory;
+import sonia.scm.repository.RepositoryLocationResolver;
 import sonia.scm.version.Version;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Extension
 public class AddTypeToCommentUpdateStep implements UpdateStep {
 
-  private static final Logger LOG = LoggerFactory.getLogger(AddTypeToCommentUpdateStep.class);
-  private static final String STORE_NAME = "pullRequestComment";
-  private final RepositoryDAO repositoryDAO;
-  private final DataStoreFactory dataStoreFactory;
+  private RepositoryLocationResolver.RepositoryLocationResolverInstance<Path> locationResolver;
 
   @Inject
-  public AddTypeToCommentUpdateStep(XmlRepositoryDAO repositoryDAO, DataStoreFactory dataStoreFactory) {
-    this.repositoryDAO = repositoryDAO;
-    this.dataStoreFactory = dataStoreFactory;
+  public AddTypeToCommentUpdateStep(RepositoryLocationResolver locationResolver) {
+    this.locationResolver = locationResolver.forClass(Path.class);
   }
 
   @Override
   public void doUpdate() {
-    for (Repository repository : repositoryDAO.getAll()) {
-      DataStore<PullRequestComments> dataStore = dataStoreFactory.withType(PullRequestComments.class).withName(STORE_NAME).forRepository(repository).build();
-      LOG.debug("update pullrequest comments for repository with id {}", repository.getId());
-      dataStore.getAll().forEach((key, properties) -> properties.getComments().forEach(comment -> {
-          if (comment.getType() == null) {
-            comment.setType(CommentType.COMMENT);
-          }
-          dataStore.put(key, properties);
-        })
-      );
+    locationResolver.forAllLocations((repositoryId, path) -> {
+      Path storePath = path.resolve("store").resolve("data").resolve("pullRequestComment");
+      if (Files.isDirectory(storePath)) {
+        try {
+          updateStorePath(storePath);
+        } catch (IOException e) {
+          throw new UpdateException("could not update path " + storePath, e);
+        }
+      }
+    });
+  }
+
+  private void updateStorePath(Path storePath) throws IOException {
+    try (Stream<Path> files = Files.list(storePath)) {
+      files.forEach(storeFile -> {
+        try {
+          updateStoreFile(storeFile);
+        } catch (ParserConfigurationException | IOException | SAXException | TransformerException e) {
+          throw new UpdateException("could not parse comment file " + storeFile + " in path " + storePath, e);
+        }
+      });
     }
+  }
+
+  private void updateStoreFile(Path storeFile) throws ParserConfigurationException, IOException, SAXException, TransformerException {
+    Document document = createDocumentBuilder().parse(storeFile.toFile());
+
+    List<Node> nodes = extractCommentNodes(document);
+    nodes.stream()
+      .filter(this::hasNoType)
+      .forEach(node -> addCommentType(document, node));
+    writeChangedNodes(storeFile, document, nodes);
+  }
+
+  private boolean hasNoType(Node commentNode) {
+    NodeList childNodes = commentNode.getChildNodes();
+    for (int i = 0; i < childNodes.getLength(); ++i) {
+      if (childNodes.item(i).getNodeName().equals("type")) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private void writeChangedNodes(Path storeFile, Document document, List<Node> nodes) throws TransformerException {
+    if (!nodes.isEmpty()) {
+      Transformer transformer = createTransformer();
+      transformer.transform(new DOMSource(document), new StreamResult(storeFile.toFile()));
+    }
+  }
+
+  private Transformer createTransformer() throws TransformerConfigurationException {
+    TransformerFactory factory = TransformerFactory.newInstance();
+    factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+    Transformer transformer = factory.newTransformer();
+    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+    return factory.newTransformer();
+  }
+
+  private List<Node> extractCommentNodes(Document document) {
+    NodeList commentNodeList = document.getDocumentElement().getElementsByTagName("comment");
+
+    List<Node> commentNodes = new ArrayList<>();
+    for (int i = 0; i < commentNodeList.getLength(); ++i) {
+      commentNodes.add(commentNodeList.item(i));
+    }
+    return commentNodes;
+  }
+
+  private void addCommentType(Document document, Node commentNode) {
+    Element newtypeNode = document.createElement("type");
+    newtypeNode.setTextContent("COMMENT");
+    commentNode.appendChild(newtypeNode);
+  }
+
+  private DocumentBuilder createDocumentBuilder() throws ParserConfigurationException {
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+    return factory.newDocumentBuilder();
   }
 
   @Override
