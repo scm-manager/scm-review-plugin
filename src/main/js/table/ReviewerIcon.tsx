fee6c7f6746311742e57f2e0e633cd2ab5c8881a
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-12-04 15:26:32 +0100

    use tooltip instead of title

--- src/main/js/table/ReviewerIcon.tsx
+++ src/main/js/table/ReviewerIcon.tsx
@@ -1,26 +1,38 @@
-import React, {FC} from "react";
-import { Icon } from "@scm-manager/ui-components";
-import {Reviewer} from "../types/PullRequest";
+import React, { FC } from "react";
+import { Icon, Tooltip } from "@scm-manager/ui-components";
+import { Reviewer } from "../types/PullRequest";
 
 type Props = {
   reviewers?: Reviewer[];
 };
 
+const chooseIcon = (length: number): string => {
+  switch (length) {
+    case 1:
+      return "user";
+    case 2:
+      return "user-friends";
+    default:
+      return "users";
+  }
+};
+
+const createTooltipMessage = (reviewers: Reviewer[]) => {
+  return reviewers.map(r => r.displayName).join(", ");
+};
+
 const ReviewerIcon: FC<Props> = ({ reviewers }) => {
-  if (!reviewers) {
+  if (!reviewers || reviewers.length === 0) {
     return null;
   }
 
-  switch (reviewers.length) {
-    case 0:
-      return null;
-    case 1:
-      return <Icon title={reviewers[0].displayName} name="user" />;
-    case 2:
-      return <Icon title={reviewers.map(r => r.displayName).join(", ")} name="user-friends" />;
-    default:
-      return <Icon title={reviewers.map(r => r.displayName).join(", ")} name="users" />;
-  }
+  const icon = chooseIcon(reviewers.length);
+  const message = createTooltipMessage(reviewers);
+  return (
+    <Tooltip location="top" message={message}>
+      <Icon name={icon} />
+    </Tooltip>
+  );
 };
 
 export default ReviewerIcon;
