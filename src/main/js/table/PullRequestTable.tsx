aaa1fcbf56d63de80218ce73d89d2e6213336579
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-12-04 14:38:55 +0100

    make all columns sortable in pull request table

--- src/main/js/table/PullRequestTable.tsx
+++ src/main/js/table/PullRequestTable.tsx
@@ -16,22 +16,34 @@ class PullRequestTable extends React.Component<Props> {
     return `/repo/${repository.namespace}/${repository.name}/pull-request/${pullRequest.id}/comments/`;
   };
 
+  createComparator = (key: string) => {
+    return (a: any, b: any) => {
+      if (a[key] > b[key]) {
+        return -1;
+      } else if (a[key] < b[key]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+  };
+
   render() {
     const { pullRequests, t } = this.props;
     return (
       <Table data={pullRequests} emptyMessage={t("scm-review-plugin.noRequests")}>
-        <Column header={t("scm-review-plugin.pullRequest.title")}>
+        <Column header={t("scm-review-plugin.pullRequest.title")} createComparator={() => this.createComparator("title")}>
           {(row: any) => <Link to={this.to(row)}>{row.title}</Link>}
         </Column>
         <TextColumn header={t("scm-review-plugin.pullRequest.sourceBranch")} dataKey="source" />
         <TextColumn header={t("scm-review-plugin.pullRequest.targetBranch")} dataKey="target" />
-        <Column header={t("scm-review-plugin.pullRequest.author")}>
+        <Column header={t("scm-review-plugin.pullRequest.author")} createComparator={() => this.createComparator("author")}>
           {(row: any) => <p>{row.author.displayName}</p>}
         </Column>
-        <Column header={t("scm-review-plugin.pullRequest.date")}>
+        <Column header={t("scm-review-plugin.pullRequest.date")} createComparator={() => this.createComparator("creationDate")}>
           {(row: any) => (row.creationDate ? <DateFromNow date={row.creationDate} /> : "")}
         </Column>
-        <Column header={t("scm-review-plugin.pullRequest.status")}>
+        <Column header={t("scm-review-plugin.pullRequest.status")} createComparator={() => this.createComparator("status")}>
           {(row: any) => (
             <Tag
               className="is-medium"
