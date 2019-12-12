c2ab86d6028b718723a162b90384ecc9d90d4530
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-12-12 10:37:13 +0100

    append tasks to pr

---
+++ src/main/java/com/cloudogu/scm/review/pullrequest/dto/TasksDto.java
@@ -0,0 +1,15 @@
+package com.cloudogu.scm.review.pullrequest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TasksDto {
+  Long done;
+  Long total;
+}
