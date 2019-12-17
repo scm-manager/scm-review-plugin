b7b5141b2c96687b335de07a86a733b9b4d9bb89
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2019-12-17 15:04:24 +0100

    remove unused imports

--- src/main/java/com/cloudogu/scm/review/events/EventListener.java
+++ src/main/java/com/cloudogu/scm/review/events/EventListener.java
@@ -1,20 +1,12 @@
 package com.cloudogu.scm.review.events;
 
-import com.cloudogu.scm.review.comment.service.BasicComment;
-import com.cloudogu.scm.review.comment.service.BasicCommentEvent;
-import com.cloudogu.scm.review.comment.service.CommentEvent;
-import com.cloudogu.scm.review.comment.service.ReplyEvent;
 import com.cloudogu.scm.review.pullrequest.service.BasicPullRequestEvent;
 import com.cloudogu.scm.review.pullrequest.service.PullRequest;
-import com.cloudogu.scm.review.pullrequest.service.PullRequestEvent;
 import com.github.legman.Subscribe;
-import lombok.Data;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import sonia.scm.EagerSingleton;
-import sonia.scm.HandlerEventType;
-import sonia.scm.event.HandlerEvent;
 import sonia.scm.plugin.Extension;
 import sonia.scm.repository.Repository;
 import sonia.scm.security.SessionId;
