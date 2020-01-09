9e1a9e6777a596cb1dd704bb5eb92d26550a4323
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2020-01-09 16:10:25 +0100

    Add hint for conflicts

--- src/main/js/MergeConflicts.tsx
+++ src/main/js/MergeConflicts.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { WithTranslation, withTranslation } from "react-i18next";
 import { Conflict, Conflicts, PullRequest } from "./types/PullRequest";
-import { Loading, Diff, DiffFile } from "@scm-manager/ui-components";
-import { Repository, Link } from "@scm-manager/ui-types";
+import { Loading, Notification, DiffFile } from "@scm-manager/ui-components";
+import { Repository } from "@scm-manager/ui-types";
 import { fetchConflicts } from "./pullRequest";
 // @ts-ignore
 import parser from "gitdiff-parser";
+import ManualMergeInformation from "./ManualMergeInformation";
 
 type Props = WithTranslation & {
   repository: Repository;
@@ -18,13 +19,15 @@ type State = {
   loading: boolean;
   error?: Error;
   conflicts?: Conflicts;
+  mergeInformation: boolean;
 };
 
 class MergeConflicts extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      loading: true
+      loading: true,
+      mergeInformation: false
     };
   }
 
@@ -77,11 +80,34 @@ class MergeConflicts extends React.Component<Props, State> {
   };
 
   render() {
-    if (this.state.loading) {
+    const { loading, conflicts, mergeInformation } = this.state;
+    const { repository, pullRequest } = this.props;
+
+    if (loading) {
       return <Loading />;
     }
 
-    return this.state.conflicts.conflicts.map(conflict => this.createDiffComponent(conflict));
+    return (
+      <>
+        <Notification type={"warning"}>
+          <div className={"content"}>
+            <b>{this.props.t("scm-review-plugin.conflicts.hint.header")}</b>
+            <p>
+              <a onClick={() => this.setState({ mergeInformation: true })}>
+                {this.props.t("scm-review-plugin.conflicts.hint.text")}
+              </a>
+            </p>
+          </div>
+        </Notification>
+        {conflicts.conflicts.map(conflict => this.createDiffComponent(conflict))}
+        <ManualMergeInformation
+          showMergeInformation={mergeInformation}
+          repository={repository}
+          pullRequest={pullRequest}
+          onClose={() => this.setState({ mergeInformation: false })}
+        />
+      </>
+    );
   }
 }
 
