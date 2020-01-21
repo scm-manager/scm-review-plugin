35b4c1e4f60eced9f452bd01ca64c66024321df6
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2020-01-21 17:32:08 +0100

    Ignore commits without new changesets

--- src/main/java/com/cloudogu/scm/review/RepositoryHook.java
+++ src/main/java/com/cloudogu/scm/review/RepositoryHook.java
@@ -5,17 +5,12 @@ import com.github.legman.Subscribe;
 import lombok.extern.slf4j.Slf4j;
 import sonia.scm.EagerSingleton;
 import sonia.scm.plugin.Extension;
-import sonia.scm.repository.Changeset;
 import sonia.scm.repository.PreReceiveRepositoryHookEvent;
 import sonia.scm.repository.Repository;
-import sonia.scm.repository.api.HookBranchProvider;
 import sonia.scm.repository.api.HookContext;
 import sonia.scm.repository.api.HookFeature;
 
 import javax.inject.Inject;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Extension
@@ -36,7 +31,7 @@ public class RepositoryHook {
     try {
       r.run();
     } finally {
-      mergeRunning.set(null);
+      mergeRunning.remove();
     }
   }
 
@@ -67,19 +62,21 @@ public class RepositoryHook {
     }
 
     log.trace("received hook for repository {}", repository.getName());
-    List<String> branches = getBranches(context, repository);
-    for (String branch : branches) {
+    for (String branch : context.getBranchProvider().getCreatedOrModified()) {
+      if (service.isBranchProtected(repository, branch)) {
+        context.getChangesetProvider().getChangesets().forEach(
+          changeset -> {
+            if (changeset.getBranches().contains(branch) && !changeset.getParents().isEmpty()) {
+              throw new BranchOnlyWritableByMergeException(repository, branch);
+            }
+          }
+        );
+      }
+    }
+    for (String branch : context.getBranchProvider().getDeletedOrClosed()) {
       if (service.isBranchProtected(repository, branch)) {
         throw new BranchOnlyWritableByMergeException(repository, branch);
       }
     }
-  }
-
-  private List<String> getBranches(HookContext eventContext, Repository repository) {
-    HookBranchProvider branchProvider = eventContext.getBranchProvider();
-    LinkedList<String> branches = new LinkedList<>();
-    branches.addAll(branchProvider.getCreatedOrModified());
-    branches.addAll(branchProvider.getDeletedOrClosed());
-    return branches;
   }
 }
--- src/test/java/com/cloudogu/scm/review/RepositoryHookTest.java
+++ src/test/java/com/cloudogu/scm/review/RepositoryHookTest.java
@@ -1,21 +1,27 @@
 package com.cloudogu.scm.review;
 
 import com.cloudogu.scm.review.config.service.ConfigService;
+import junit.framework.AssertionFailedError;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import sonia.scm.repository.Changeset;
 import sonia.scm.repository.PreReceiveRepositoryHookEvent;
 import sonia.scm.repository.Repository;
 import sonia.scm.repository.api.HookBranchProvider;
+import sonia.scm.repository.api.HookChangesetBuilder;
 import sonia.scm.repository.api.HookContext;
 import sonia.scm.repository.api.HookFeature;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,10 +33,12 @@ class RepositoryHookTest {
   ConfigService configService;
   @Mock
   PreReceiveRepositoryHookEvent event;
+  @InjectMocks
+  RepositoryHook hook;
 
   @Test
   void shouldNotFailOnEmptyContext() {
-    new RepositoryHook(configService).onEvent(event);
+    hook.onEvent(event);
   }
 
   @Nested
@@ -39,6 +47,8 @@ class RepositoryHookTest {
     HookContext hookContext;
     @Mock
     HookBranchProvider branchProvider;
+    @Mock
+    HookChangesetBuilder changesetBuilder;
 
     @BeforeEach
     void setContext() {
@@ -46,13 +56,14 @@ class RepositoryHookTest {
       when(hookContext.isFeatureSupported(HookFeature.BRANCH_PROVIDER)).thenReturn(true);
       when(event.getRepository()).thenReturn(REPOSITORY);
       lenient().when(hookContext.getBranchProvider()).thenReturn(branchProvider);
+      lenient().when(hookContext.getChangesetProvider()).thenReturn(changesetBuilder);
     }
 
     @Test
     void shouldIgnoreDisabledConfig() {
       when(configService.isEnabled(REPOSITORY)).thenReturn(false);
 
-      new RepositoryHook(configService).onEvent(event);
+      hook.onEvent(event);
     }
 
     @Test
@@ -62,19 +73,40 @@ class RepositoryHookTest {
 
       when(branchProvider.getCreatedOrModified()).thenReturn(asList("feature"));
 
-      new RepositoryHook(configService).onEvent(event);
+      hook.onEvent(event);
     }
 
     @Test
-    void shouldFailForProtectedBranch() {
+    void shouldIgnoreBranchesWithoutChangesets() {
       when(configService.isEnabled(REPOSITORY)).thenReturn(true);
       when(configService.isBranchProtected(REPOSITORY, "master")).thenReturn(true);
 
       when(branchProvider.getCreatedOrModified()).thenReturn(asList("master"));
+      when(changesetBuilder.getChangesets()).thenReturn(emptyList());
+
+      hook.onEvent(event);
+    }
+
+    @Test
+    void shouldFailForProtectedBranchWithChangeset() {
+      when(configService.isEnabled(REPOSITORY)).thenReturn(true);
+      when(configService.isBranchProtected(REPOSITORY, "master")).thenReturn(true);
+
+      when(branchProvider.getCreatedOrModified()).thenReturn(asList("master"));
+      Changeset changeset = mock(Changeset.class);
+      when(changesetBuilder.getChangesets()).thenReturn(asList(changeset));
+      when(changeset.getBranches()).thenReturn(asList("master"));
+      when(changeset.getParents()).thenReturn(asList("parent"));
 
       Assertions.assertThrows(BranchOnlyWritableByMergeException.class, () ->
-        new RepositoryHook(configService).onEvent(event)
+        hook.onEvent(event)
       );
     }
+  }
+
+  @Test
+  void shouldIgnoreChecksWhenRunPrivileged() {
+    lenient().when(event.getContext()).thenThrow(new AssertionFailedError("this should not have been called"));
+    hook.runPrivileged(() -> hook.onEvent(event));
   }
 }
