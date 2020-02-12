dfbf4523b9e2ea93bf2b35f97db0a992a92bc5fd
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2020-02-12 14:55:55 +0100

    add custom markdown formatting for comments

--- src/main/js/comment/CommentContent.tsx
+++ src/main/js/comment/CommentContent.tsx
@@ -2,6 +2,36 @@ import React, { FC, useEffect, useState } from "react";
 import { Comment } from "../types/PullRequest";
 import { useTranslation } from "react-i18next";
 import { MarkdownView } from "@scm-manager/ui-components";
+import styled from "styled-components";
+
+const MarkdownWrapper = styled.div`
+  .content {
+    > h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      margin: 0.5rem 0 !important;
+      font-size: 0.9rem !important;
+    }
+    > h1 {
+      font-weight: 700;
+    }
+    > h2 {
+      font-weight: 600;
+    }
+    > h3,
+    h4,
+    h5,
+    h6 {
+      font-weight: 500;
+    }
+    & strong {
+      font-weight: 500;
+    }
+  }
+`;
 
 type Props = {
   comment: Comment;
@@ -15,13 +45,17 @@ const CommentContent: FC<Props> = ({ comment }) => {
   useEffect(() => {
     let content = message;
     comment.mentions.forEach(m => {
-      const matcher = new RegExp("@\\[" + m.id + "\\]", 'g');
+      const matcher = new RegExp("@\\[" + m.id + "\\]", "g");
       content = content.replace(matcher, `[${m.displayName}](mailto:${m.mail})`);
     });
     setMessage(content);
   }, [comment]);
 
-  return <MarkdownView content={message} />;
+  return (
+    <MarkdownWrapper>
+      <MarkdownView content={message} />
+    </MarkdownWrapper>
+  );
 };
 
 export default CommentContent;
