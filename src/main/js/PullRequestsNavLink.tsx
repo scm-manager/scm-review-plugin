5c94ca88f927e4937a596c32fe502c718d1d6c2d
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2020-03-11 16:12:10 +0100

    Use secondary navigation component

--- src/main/js/PullRequestsNavLink.tsx
+++ src/main/js/PullRequestsNavLink.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink, MenuContext } from "@scm-manager/ui-components";
+import { SecondaryNavigationItem } from "@scm-manager/ui-components";
 import { WithTranslation, withTranslation } from "react-i18next";
 
 type Props = WithTranslation & {
@@ -12,18 +12,13 @@ class PullRequestsNavLink extends React.Component<Props> {
     const { url, activeWhenMatch, t } = this.props;
 
     return (
-      <MenuContext.Consumer>
-        {({ menuCollapsed }) => (
-          <NavLink
-            to={`${url}/pull-requests`}
-            icon="fas fa-code-branch fa-rotate-180"
-            label={t("scm-review-plugin.navLink")}
-            activeWhenMatch={activeWhenMatch}
-            title={t("scm-review-plugin.navLink")}
-            collapsed={menuCollapsed}
-          />
-        )}
-      </MenuContext.Consumer>
+      <SecondaryNavigationItem
+        to={`${url}/pull-requests`}
+        icon="fas fa-code-branch fa-rotate-180"
+        label={t("scm-review-plugin.navLink")}
+        activeWhenMatch={activeWhenMatch}
+        title={t("scm-review-plugin.navLink")}
+      />
     );
   }
 }
