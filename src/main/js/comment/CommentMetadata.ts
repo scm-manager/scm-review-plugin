3df7e9bc9777ceee1c92d98db28c342bd61863bd
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2020-03-27 08:01:12 +0100

    Fix license headers

--- src/main/js/comment/CommentMetadata.ts
+++ src/main/js/comment/CommentMetadata.ts
@@ -20,7 +20,8 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
- */import React from "react";
+ */
+import React from "react";
 import styled from "styled-components";
 
 const CommentMetadata = styled.span`
