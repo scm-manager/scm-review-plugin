c3d87b348f3c64fed37a49109b22a1f29a46b88a
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2021-12-06 16:20:54 +0100

    Style popover and some refactoring

--- src/main/js/BranchDetailsPullRequests.tsx
+++ src/main/js/BranchDetailsPullRequests.tsx
@@ -23,8 +23,16 @@
  */
 
 import React, { FC } from "react";
-import { Branch, BranchDetails, HalRepresentation, Repository } from "@scm-manager/ui-types";
-import { Icon, SmallLoadingSpinner, usePopover, Popover } from "@scm-manager/ui-components";
+import { Branch, BranchDetails, Repository } from "@scm-manager/ui-types";
+import {
+  Icon,
+  SmallLoadingSpinner,
+  usePopover,
+  Popover,
+  CardColumnSmall,
+  DateFromNow,
+  AvatarImage
+} from "@scm-manager/ui-components";
 import { useHistory } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
@@ -34,10 +42,67 @@ const StyledText = styled.span.attrs(() => ({
   className: "has-text-grey is-size-7"
 }))``;
 
+const UnbreakableText = styled.span`
+  word-break: keep-all;
+`;
+
 type Props = {
   repository: Repository;
   branch: Branch;
   details?: BranchDetails;
+};
+
+type PRListProps = {
+  repository: Repository;
+  pullRequests: PullRequest[];
+};
+
+const PullRequestList: FC<PRListProps> = ({ repository, pullRequests }) => {
+  const [t] = useTranslation("plugins");
+
+  return (
+    <>
+      {pullRequests.map((pr, key) => {
+        if (key > 2) {
+          return null;
+        }
+        return (
+          <>
+            <CardColumnSmall
+              key={key}
+              link={`/repo/${repository.namespace}/${repository.name}/pull-request/${pr.id}`}
+              contentLeft={
+                <div className="has-text-grey pb-2">
+                  <strong>{pr.title}</strong>
+                  <div className="is-flex is-justify-content-space-between">
+                    <div className="is-flex is-flex-direction-row is-align-items-center is-justify-content-center">
+                      <AvatarImage
+                        className="image is-16x16 mr-1"
+                        person={{ name: pr.author?.displayName || "", mail: pr.author?.mail }}
+                        representation="rounded-border"
+                      />
+                      <span className="has-size-7">{pr.author?.displayName || pr.author?.mail || ""}</span>
+                    </div>
+                    <DateFromNow date={pr.creationDate} className="is-justify-content-flex-end" />
+                  </div>
+                </div>
+              }
+              contentRight={
+                <div className="has-text-grey is-flex is-justify-content-space-between">
+                  <UnbreakableText className="pr-1">
+                    {t("scm-review-plugin.branchDetails.pullRequest.targetBranch")}
+                    {": "}
+                  </UnbreakableText>
+                  <span className="is-flex-grow-0 is-ellipsis-overflow">{pr.target}</span>
+                </div>
+              }
+            />
+            {key < pullRequests.length - 1 && key < 2 ? <hr className="m-0" /> : null}
+          </>
+        );
+      })}
+    </>
+  );
 };
 
 const BranchDetailsPullRequests: FC<Props> = ({ repository, branch, details }) => {
@@ -53,8 +118,7 @@ const BranchDetailsPullRequests: FC<Props> = ({ repository, branch, details }) =
     return <SmallLoadingSpinner />;
   }
 
-  const prs: PullRequest[] =
-    ((details._embedded?.pullRequests as HalRepresentation)?._embedded?.pullRequests as PullRequest[]) || [];
+  const prs: PullRequest[] = (details._embedded?.pullRequests as PullRequest[]) || [];
 
   if (prs.length > 0) {
     return (
@@ -65,10 +129,10 @@ const BranchDetailsPullRequests: FC<Props> = ({ repository, branch, details }) =
               {t("scm-review-plugin.branchDetails.pullRequest.popover")}
             </h1>
           }
-          width={500}
+          width={400}
           {...popoverProps}
         >
-          abc
+          <PullRequestList repository={repository} pullRequests={prs} />
         </Popover>
         <div {...triggerProps}>
           <Icon name="code-branch" className="is-size-7" />
