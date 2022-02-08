3f5632ab4df24cab13e26a276418dc240b247070
Matthias Thieroff <matthias.thieroff@cloudogu.com>
2022-02-08 13:07:32 +0100

    Remove console.log()

--- src/main/js/MergeModal.tsx
+++ src/main/js/MergeModal.tsx
@@ -87,7 +87,6 @@ const MergeModal: FC<Props> = ({ pullRequest, emergencyMerge, close, merge, merg
       setLoadingDefaultMessage(true);
       getMergeStrategyInfo((pullRequest._links.mergeStrategyInfo as Link).href + "?strategy=" + mergeStrategy)
         .then((commitStrategyInfo: MergeStrategyInfo) => {
-          console.log("NEW MERGE STRATEGY INFO", commitStrategyInfo);
           commitStrategyInfos[mergeStrategy] = commitStrategyInfo;
           setCommitStrategyInfos(commitStrategyInfos);
           setMergeCommit({ ...mergeCommit, commitMessage: commitStrategyInfo.defaultCommitMessage });
