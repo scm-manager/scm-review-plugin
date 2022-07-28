0c8f7ee08cf00248c290bb4b4bdf0909b7b977cc
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2022-07-28 17:40:36 +0200

    add extension point to pr table

--- src/main/js/table/PullRequestTable.tsx
+++ src/main/js/table/PullRequestTable.tsx
@@ -30,6 +30,8 @@ import { Column, comparators, DateFromNow, Table, Tag, TextColumn } from "@scm-m
 import { Link } from "react-router-dom";
 import ReviewerIcon from "./ReviewerIcon";
 import PullRequestStatusTag from "../PullRequestStatusTag";
+import { binder } from "@scm-manager/ui-extensions";
+import { PullRequestTableColumn } from "../types/ExtensionPoints";
 
 type Props = WithTranslation & {
   repository: Repository;
@@ -54,8 +56,8 @@ class PullRequestTable extends React.Component<Props> {
 
   todoTag = (pullRequest: PullRequest) => {
     const { t } = this.props;
-    const todos = pullRequest.tasks.todo;
-    if (todos === 0) {
+    const todos = pullRequest.tasks?.todo;
+    if (!todos) {
       return null;
     }
     return (
@@ -68,43 +70,55 @@ class PullRequestTable extends React.Component<Props> {
   };
 
   render() {
-    const { pullRequests, t } = this.props;
+    const { repository, pullRequests, t } = this.props;
+    const baseColumns = [
+      <Column
+        header={t("scm-review-plugin.pullRequest.title")}
+        createComparator={() => comparators.byKey("title")}
+        ascendingIcon="sort-alpha-down-alt"
+        descendingIcon="sort-alpha-down"
+      >
+        {(row: PullRequest) => <Link to={this.to(row)}>{row.title}</Link>}
+      </Column>,
+      <Column header="">{(row: PullRequest) => <>{this.todoTag(row)}</>}</Column>,
+      <TextColumn header={t("scm-review-plugin.pullRequest.sourceBranch")} dataKey="source" />,
+      <TextColumn header={t("scm-review-plugin.pullRequest.targetBranch")} dataKey="target" />,
+      <MobileHiddenColumn
+        header={t("scm-review-plugin.pullRequest.author")}
+        createComparator={() => comparators.byNestedKeys("author", "displayName")}
+        ascendingIcon="sort-alpha-down-alt"
+        descendingIcon="sort-alpha-down"
+      >
+        {(row: PullRequest) => <p>{row.author?.displayName}</p>}
+      </MobileHiddenColumn>,
+      <MobileHiddenColumn
+        header={t("scm-review-plugin.pullRequest.date")}
+        createComparator={() => comparators.byKey("creationDate")}
+        ascendingIcon="sort-amount-down-alt"
+        descendingIcon="sort-amount-down"
+      >
+        {(row: PullRequest) => (row.creationDate ? <DateFromNow date={row.creationDate} /> : "")}
+      </MobileHiddenColumn>,
+      <MobileHiddenColumn header="">
+        {(row: PullRequest) => <ReviewerIcon reviewers={row.reviewer} />}
+      </MobileHiddenColumn>,
+      <MobileHiddenColumn
+        header={t("scm-review-plugin.pullRequest.status")}
+        createComparator={() => comparators.byKey("status")}
+      >
+        {(row: PullRequest) => <PullRequestStatusTag status={row.status} emergencyMerged={row.emergencyMerged} />}
+      </MobileHiddenColumn>
+    ];
+    const additionalColumns = binder.getExtensions<PullRequestTableColumn>("pull-requests.table.column").map(ext =>
+      ext({
+        repository,
+        t
+      })
+    );
+    const columns = baseColumns.concat(...additionalColumns);
     return (
       <StyledTable data={pullRequests} emptyMessage={t("scm-review-plugin.noRequests")}>
-        <Column
-          header={t("scm-review-plugin.pullRequest.title")}
-          createComparator={() => comparators.byKey("title")}
-          ascendingIcon="sort-alpha-down-alt"
-          descendingIcon="sort-alpha-down"
-        >
-          {(row: any) => <Link to={this.to(row)}>{row.title}</Link>}
-        </Column>
-        <Column header="">{(row: any) => <>{this.todoTag(row)}</>}</Column>
-        <TextColumn header={t("scm-review-plugin.pullRequest.sourceBranch")} dataKey="source" />
-        <TextColumn header={t("scm-review-plugin.pullRequest.targetBranch")} dataKey="target" />
-        <MobileHiddenColumn
-          header={t("scm-review-plugin.pullRequest.author")}
-          createComparator={() => comparators.byNestedKeys("author", "displayName")}
-          ascendingIcon="sort-alpha-down-alt"
-          descendingIcon="sort-alpha-down"
-        >
-          {(row: any) => <p>{row.author.displayName}</p>}
-        </MobileHiddenColumn>
-        <MobileHiddenColumn
-          header={t("scm-review-plugin.pullRequest.date")}
-          createComparator={() => comparators.byKey("creationDate")}
-          ascendingIcon="sort-amount-down-alt"
-          descendingIcon="sort-amount-down"
-        >
-          {(row: any) => (row.creationDate ? <DateFromNow date={row.creationDate} /> : "")}
-        </MobileHiddenColumn>
-        <MobileHiddenColumn header="">{(row: any) => <ReviewerIcon reviewers={row.reviewer} />}</MobileHiddenColumn>
-        <MobileHiddenColumn
-          header={t("scm-review-plugin.pullRequest.status")}
-          createComparator={() => comparators.byKey("status")}
-        >
-          {(row: any) => <PullRequestStatusTag status={row.status} emergencyMerged={row.emergencyMerged} />}
-        </MobileHiddenColumn>
+        {columns}
       </StyledTable>
     );
   }
